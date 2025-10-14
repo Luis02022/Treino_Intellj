729d4de36a92f9281709d605daa2f7642a696854
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2025-10-14 13:01:33 -0300

    Delete treinamento-05 directory

--- treinamento-05/src/main/java/com/example/treino/treinamento/_5/repository/PlanoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.treino.treinamento._5.repository;
-
-import com.example.treino.treinamento._5.model.Plano;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanoRepository extends JpaRepository<Plano, Long> {
-}
